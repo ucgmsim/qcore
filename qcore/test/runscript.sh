f62b7de8f33a02eac038f182c7ae119235e030ff
sharmiamala <sharmila.savarimuthu@canterbury.ac.nz>
2018-05-01 14:36:16 +1200

    runscript added

--- qcore/test/runscript.sh
+++ qcore/test/runscript.sh
@@ -1,7 +1,19 @@
 echo "from run script"
+DIRS='test_*/'
+curdir=$PWD
+for DIR in $DIRS
+do
+cd ${curdir}/${DIR}
+pwd
+echo ${DIR}
+pytest -s -v
+done
 
-py.test --junitxml results.xml qcore/test/test_geo.py
 
+#py.test --junitxml results.xml qcore/test/test_geo/test_geo.py
 
-python -m coverage run qcore/test/test_geo.py
-python -m coverage xml -o coverage.xml
+#python -m coverage run qcore/test/test_geo/test_geo.py
+#python -m coverage xml -o coverage.xml
+
+# py.test --junitxml results.xml
+# cp -r ./results.xml /var/lib/jenkins/workspace/pytest_workflow/results.xml
