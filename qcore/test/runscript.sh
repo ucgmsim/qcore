a255a1a95aff51ffdb745222871ffcd514118541
sharmiamala <sharmila.savarimuthu@canterbury.ac.nz>
2018-05-02 15:25:13 +1200

    modified runscript

--- qcore/test/runscript.sh
+++ qcore/test/runscript.sh
@@ -6,10 +6,13 @@ do
 cd ${curdir}/${DIR}
 pwd
 echo ${DIR}
-pytest --junitxml results.xml
+pytest --junitxml ${DIR}.xml
 done
 
-cp -r ./results.xml /home/aas105/.jenkins/workspace/testtrial/results.xml
+cp -r ./test_gen_coords.xml /home/aas105/.jenkins/workspace/testtrial/test_gen_coords.xml
+cp -r ./test_geo.xml /home/aas105/.jenkins/workspace/testtrial/test_geo.xml
+cp -r ./test_srf.xml /home/aas105/.jenkins/workspace/testtrial/test_srf.xml
+cp -r ./test_xyts.xml /home/aas105/.jenkins/workspace/testtrial/test_xyts.xml
 
 #py.test --junitxml results.xml qcore/test/test_geo/test_geo.py
 
