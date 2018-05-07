eecfc4781471de92aea476d5feae742f0abba005
sharmiamala <sharmila.savarimuthu@canterbury.ac.nz>
2018-05-08 11:30:44 +1200

    changes in runscript

--- qcore/test/runscript.sh
+++ qcore/test/runscript.sh
@@ -2,14 +2,15 @@
 printf "FROM RUNSCRIPT.SH"
 DIRS='test_*'
 curdir=$PWD
-for DIR in $DIRS
 printf "___________________________________________________________________________________________________________________________\r"
+for DIR in $DIRS
 do
 cd ${curdir}/${DIR}
 pytest --junitxml ${DIR}.xml
 cp -r ./${DIR}.xml /home/aas105/.jenkins/workspace/qcore-tests/${DIR}.xml
-done
 printf "___________________________________________________________________________________________________________________________\r"
+done
+
 
 #py.test --junitxml results.xml qcore/test/test_geo/test_geo.py
 
