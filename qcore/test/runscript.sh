ce6c41253fc3e688a7fb7c1a52c85f39e4c7037b
sharmiamala <sharmila.savarimuthu@canterbury.ac.nz>
2018-05-09 09:04:34 +1200

    removed log.html

--- qcore/test/runscript.sh
+++ qcore/test/runscript.sh
@@ -7,7 +7,5 @@ pytest --junitxml ${DIR}.xml
 cp -r ./${DIR}.xml /home/aas105/.jenkins/workspace/qcore-tests/${DIR}.xml
 done
 LOG=/home/aas105/.jenkins/jobs/qcore-tests/builds/$BUILD_NUMBER/log
-cp ${LOG} ${LOG}.html
-sed -i 's/$/<br>/' ${LOG}.html
-cp -r ${LOG}.html /home/aas105/.jenkins/workspace/qcore-tests/${LOG}.html
-cat ${LOG}.html
+sed -i 's/$/<br>/' ${LOG}
+
