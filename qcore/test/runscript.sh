bcb1f65b6cbc3d8f97fcd328537a36b6dd239028
sharmiamala <sharmila.savarimuthu@canterbury.ac.nz>
2018-05-08 16:51:58 +1200

    added sed command

--- qcore/test/runscript.sh
+++ qcore/test/runscript.sh
@@ -2,17 +2,11 @@ DIRS='test_*'
 curdir=$PWD
 for DIR in $DIRS
 do
-printf '<br><br><br>'
 cd ${curdir}/${DIR}
 pytest --junitxml ${DIR}.xml
 cp -r ./${DIR}.xml /home/aas105/.jenkins/workspace/qcore-tests/${DIR}.xml
 done
-printf '<br><br><br>'
-
-#py.test --junitxml results.xml qcore/test/test_geo/test_geo.py
-
-#python -m coverage run qcore/test/test_geo/test_geo.py
-#python -m coverage xml -o coverage.xml
-
-# py.test --junitxml results.xml
-# cp -r ./results.xml /var/lib/jenkins/workspace/pytest_workflow/results.xml
+LOG=/home/aas105/.jenkins/jobs/qcore-tests/builds/$BUILD_NUMBER/log
+cp ${LOG} ${LOG}.html
+sed -i 's/$/<br>/' ${LOG}.html
+cat ${LOG}.html
