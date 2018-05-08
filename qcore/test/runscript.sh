54c87bc4c980b12862b01042660cbda84676dfb2
sharmiamala <sharmila.savarimuthu@canterbury.ac.nz>
2018-05-08 17:03:14 +1200

    added dev null

--- qcore/test/runscript.sh
+++ qcore/test/runscript.sh
@@ -3,7 +3,7 @@ curdir=$PWD
 for DIR in $DIRS
 do
 cd ${curdir}/${DIR}
-pytest --junitxml ${DIR}.xml
+pytest --junitxml ${DIR}.xml >/dev/null
 cp -r ./${DIR}.xml /home/aas105/.jenkins/workspace/qcore-tests/${DIR}.xml
 done
 LOG=/home/aas105/.jenkins/jobs/qcore-tests/builds/$BUILD_NUMBER/log
