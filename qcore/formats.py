429e18e5f737a4d8279ed4d2613fe9e779bb05bd
Viktor Polak <viktor.polak@canterbury.ac.nz>
2018-06-28 14:04:12 +1200

    fixed and/or logic

--- qcore/formats.py
+++ qcore/formats.py
@@ -2,6 +2,8 @@
 Functions and classes to load data that doesn't belong elsewhere.
 """
 
+import numpy as np
+
 def load_im_file(csv_file, all_psa = False):
 
     # process column names
@@ -11,8 +13,8 @@ def load_im_file(csv_file, all_psa = False):
         raw_cols = f.readline().strip().split(',')
     for i, c in enumerate(raw_cols):
         # filter out pSA that aren't round numbers, duplicates
-        if c not in names and \
-                all_psa or (c.startswith('pSA_') and len(c) < 12):
+        if c not in col_names and \
+                (all_psa or not (c.startswith('pSA_') and len(c) > 12)):
             use_cols.append(i)
             col_names.append(c)
 
@@ -23,5 +25,5 @@ def load_im_file(csv_file, all_psa = False):
     dtype[1] = ('component', '|S4')
 
     # load all at once
-    d = np.loadtxt(csv_file, dtype = dtype, delimiter = ',', \
-                   skiprows = 1, usecols = tuple(use_cols))
+    return np.rec.array(np.loadtxt(csv_file, dtype = dtype, delimiter = ',', \
+                                   skiprows = 1, usecols = tuple(use_cols)))
