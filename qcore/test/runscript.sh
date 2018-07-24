92d03ae2553b7458c804336c5e9da15131eff6ab
Sung Bae <sung.bae@canterbury.ac.nz>
2018-07-25 10:43:11 +1200

    changed runscript

--- qcore/test/runscript.sh
+++ qcore/test/runscript.sh
@@ -4,8 +4,8 @@ for DIR in $DIRS
 do
 cd ${curdir}/${DIR}
 pytest --junitxml ${DIR}.xml
-cp -r ./${DIR}.xml /home/aas105/.jenkins/workspace/qcore-tests/${DIR}.xml
+cp -r ./${DIR}.xml /home/tester/.jenkins/workspace/qcore-tests/${DIR}.xml
 done
-LOG=/home/aas105/.jenkins/jobs/qcore-tests/builds/$BUILD_NUMBER/log
+LOG=/home/tester/.jenkins/jobs/qcore-tests/builds/$BUILD_NUMBER/log
 sed -i 's/$/<br>/' ${LOG}
 
