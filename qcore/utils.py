e133e1715944fa6134de16302c27208e4c10da95
ttgenius <melodyzyz@gmail.com>
2018-11-02 13:22:26 +1300

    added yaml utils

--- qcore/utils.py
+++ qcore/utils.py
@@ -6,6 +6,58 @@ Mostly related to file system operations and other non-specific functionality.
 from shutil import rmtree
 import os
 import imp
+import yaml
+from collections import OrderedDict
+
+
+class DotDictify(dict):
+    MARKER = object()
+
+    def __init__(self, value=None):
+        if value is None:
+            pass
+        elif isinstance(value, dict):
+            for key in value:
+                self.__setitem__(key, value[key])
+        else:
+            raise TypeError('expected dict')
+
+    def __setitem__(self, key, value):
+        if isinstance(value, dict) and not isinstance(value, DotDictify):
+            value = DotDictify(value)
+        super(DotDictify, self).__setitem__(key, value)
+
+    def __getitem__(self, key):
+        found = self.get(key, DotDictify.MARKER)
+        if found is DotDictify.MARKER:
+            found = DotDictify()
+            super(DotDictify, self).__setitem__(key, found)
+        return found
+
+    __setattr__, __getattr__ = __setitem__, __getitem__
+
+
+def load_yaml(yaml_file):
+    with open(yaml_file, 'r') as stream:
+        try:
+            return yaml.load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+
+def dump_yaml(input_dict, output_name):
+    with open(output_name, 'w') as yaml_file:
+        yaml.add_representer(OrderedDict, lambda dumper, data: dumper.represent_mapping('tag:yaml.org,2002:map', data.items()))
+        yaml.dump(input_dict, stream=yaml_file, default_flow_style=False)
+
+
+def load_params(*yaml_files):
+    dot_dict = DotDictify({})
+    for yaml_file in yaml_files:
+        d = DotDictify(load_yaml(yaml_file))
+        dot_dict.update(d)
+    return dot_dict
+
 
 def setup_dir(directory, empty = False):
     """
@@ -27,17 +79,20 @@ def setup_dir(directory, empty = False):
             if not os.path.isdir(directory):
                 raise
 
+
 def load_py_cfg(f_path):
-	"""
-	loads a python configuration file to a dictionary
+    """
+    loads a python configuration file to a dictionary
 
         if you want to preserve the import params functionality, locals().update(cfg_dict) converts the returned dict to local variables.
 
-	:param f_path: path to configuration file
-	:return: dict of parameters
-	"""
-	with open(f_path) as f:
-		module = imp.load_module('params', f, f_path, ('.py', 'r', imp.PY_SOURCE))
-		cfg_dict = module.__dict__
-		
-	return cfg_dict
+    :param f_path: path to configuration file
+    :return: dict of parameters
+    """
+    with open(f_path) as f:
+        module = imp.load_module('params', f, f_path, ('.py', 'r', imp.PY_SOURCE))
+        cfg_dict = module.__dict__
+
+    return cfg_dict
+
+
