96410747d2b1cb5a48361e2aea13c2ec9aa8176e
Viktor Polak <viktor.polak@canterbury.ac.nz>
2018-12-18 13:31:11 +1300

    very basic version storing IMs 3x speed, 1/5 file size

--- qcore/imdb_create.py
+++ qcore/imdb_create.py
@@ -9,10 +9,10 @@ from glob import glob
 import os
 import sqlite3
 
+import h5py
 from mpi4py import MPI
 import numpy as np
 import pandas as pd
-
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -20,30 +20,15 @@ size = comm.Get_size()
 master = 0
 is_master = not rank
 
-# collect required arguements
-args = None
-csvs = None
-if is_master:
-    parser = ArgumentParser()
-    arg = parser.add_argument
-    arg("runs_dir", help="Location of Runs folder")
-    arg("station_file", help="Location of station (ll) file")
-    arg("db_file", help="Where to store IMDB")
-    try:
-        args = parser.parse_args()
-    except SystemExit:
-        # invalid arguments or -h
-        comm.Abort()
-    # add some additional info
-    csvs = glob(os.path.join(args.runs_dir, "*", "IM_calc", "*", "*.csv"))
-args = comm.bcast(args, root=master)
-csvs = comm.bcast(csvs, root=master)
-sims = list(map(lambda path: os.path.splitext(os.path.basename(path))[0], csvs))
-faults = list(map(lambda sim: sim.split("_HYP")[0], sims))
 
-###
-### PASS 1 : determine work size
-###
+def __csv_ims(csv):
+    """
+    Loads individual simulation CSV files.
+    """
+    c = pd.read_csv(csv, index_col=0)
+    return c.loc[c["component"] == "geom"].drop("component", axis="columns")
+
+
 def __csv_stations(csv):
     """
     Minimal load function that returns stations available in CSV file.
@@ -70,18 +55,85 @@ def __add_dict_counters(dict_a, dict_b, datatype):
     return dict_a
 
 
+statSumOp = MPI.Op.Create(__add_dict_counters, commute=True)
+
+
+# collect required arguements
+args = None
+csvs = None
+if is_master:
+    parser = ArgumentParser()
+    arg = parser.add_argument
+    arg("runs_dir", help="Location of Runs folder")
+    arg("station_file", help="Location of station (ll) file")
+    arg("db_file", help="Where to store IMDB")
+    try:
+        args = parser.parse_args()
+    except SystemExit:
+        # invalid arguments or -h
+        comm.Abort()
+    # add some additional info
+    csvs = glob(os.path.join(args.runs_dir, "*", "IM_calc", "*", "*.csv"))
+args = comm.bcast(args, root=master)
+csvs = comm.bcast(csvs, root=master)
+rank_csvs = csvs[rank::size]
+sims = list(map(lambda path: os.path.splitext(os.path.basename(path))[0], csvs))
+faults = list(map(lambda sim: sim.split("_HYP")[0], sims))
+
+
+###
+### PASS 1 : determine work size
+###
+
+if is_master:
+    t0 = MPI.Wtime()
+
 # determine number of simulations for each station
 stat_nsim = {}
-for csv in csvs[rank::size]:
+for csv in rank_csvs:
     for stat in __csv_stations(csv):
         try:
-            stat_nsim[stat] += 1
+            stat_nsim[stat][rank] += 1
         except KeyError:
-            stat_nsim[stat] = 1
-statSumOp = MPI.Op.Create(__add_dict_counters, commute=True)
+            stat_nsim[stat] = np.zeros(size)
+            stat_nsim[stat][rank] = 1
+# get a complete set of simulations for each station by rank
 stat_nsim = comm.allreduce(stat_nsim, op=statSumOp)
+
+# other metadata
+ims = __csv_ims(csvs[0]).columns.values
+n_im = len(ims)
+
+if is_master:
+    print("gathering metadata complete (%.2fs)" % (MPI.Wtime() - t0))
+
 
 ###
 ### PASS 2 : store data
 ###
+
+if is_master:
+    t0 = MPI.Wtime()
+
+# create structure together
 h5 = h5py.File(args.db_file, "w", driver="mpio", comm=comm)
+ims = {}
+for stat in stat_nsim:
+    ims[stat] = h5.create_dataset(
+        "station_data/%s" % (stat), (sum(stat_nsim[stat]), n_im), dtype="f4"
+    )
+if is_master:
+    print("hdf datastructures created (%.2fs)" % (MPI.Wtime() - t0))
+
+# store im values
+for i, csv in enumerate(rank_csvs):
+    print("[%03d] %03d / %03d..." % (rank, i, len(rank_csvs)))
+    df = __csv_ims(csv)
+    for stat in df.index.values:
+        stat_nsim[stat][rank] -= 1
+        ims[stat][sum(stat_nsim[stat][:rank + 1])] = df.loc[stat].values
+
+
+# will hang for a long time if datasets are not deleted
+del ims
+h5.close()
