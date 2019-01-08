97120221af000d0668a3f46ad8f385561398e762
ttgenius <melodyzyz@gmail.com>
2019-01-08 15:12:43 +1300

    moved benchmark xyts.e3d to dropbox

--- qcore/test/test_xyts/test_xyts.py
+++ qcore/test/test_xyts/test_xyts.py
@@ -2,6 +2,7 @@ import os
 from qcore import xyts
 import pytest
 from qcore.test.tool import utils
+from qcore import shared
 import numpy as np
 import getpass
 from datetime import datetime
@@ -9,15 +10,31 @@ import sys
 import shutil
 import errno
 
-XYTS_ACTUAL_PATH =os.path.join(os.path.abspath(os.path.dirname(__file__)),"xyts.e3d")
-SAMPLE_OUT_DIR_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),"sample1/output")
-XYTS_FILE = os.path.join(os.path.abspath(os.path.dirname(__file__)),"sample1/xyts.e3d")
-os.symlink(XYTS_ACTUAL_PATH, XYTS_FILE)
+
+XYTS_DOWNLOAD_PATH = 'https://www.dropbox.com/s/zge70zvntzxatpo/xyts.e3d?dl=0'
+XYTS_STORE_PATH = os.path.join('/home', getpass.getuser(), 'xyts.e3d')
+DOWNLOAD_CMD = 'wget -O {} {}'.format(XYTS_STORE_PATH, XYTS_DOWNLOAD_PATH)
+
+if not os.path.isfile(XYTS_STORE_PATH):
+   out, err = shared.exe(DOWNLOAD_CMD, debug=False)
+   if 'failed' in err:
+        os.remove(XYTS_STORE_PATH)
+        sys.exit('{} failted to download xyts benchmark file'.format(err))
+   else:
+       print("Successfully downloaded benchmark xyts.e3d")
+else:
+    print("Benchmark xyts.e3d already exits")
+
+SAMPLE_OUT_DIR_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), "sample1/output")
+XYTS_FILE = os.path.join(os.path.abspath(os.path.dirname(__file__)), "sample1/xyts.e3d")
+os.symlink(XYTS_STORE_PATH, XYTS_FILE)
 OBJ_XYTS = xyts.XYTSFile(XYTS_FILE)
-SAMPLE_PGV = os.path.join(SAMPLE_OUT_DIR_PATH,"sample_pgvout")
-SAMPLE_MMI = os.path.join(SAMPLE_OUT_DIR_PATH,"sample_mmiout")
-TMP_DIR_NAME = (os.path.join("/home/",getpass.getuser(),("tmp_" + os.path.basename(__file__)[:-3] + '_' + ''.join(str(datetime.now()).split())).replace('.', '_')).replace(
-            ':', '_'))
+SAMPLE_PGV = os.path.join(SAMPLE_OUT_DIR_PATH, "sample_pgvout")
+SAMPLE_MMI = os.path.join(SAMPLE_OUT_DIR_PATH, "sample_mmiout")
+TMP_DIR_NAME = (os.path.join("/home/", getpass.getuser(), (
+"tmp_" + os.path.basename(__file__)[:-3] + '_' + ''.join(str(datetime.now()).split())).replace('.', '_')).replace(
+    ':', '_'))
+
 
 def setup_module(scope="module"):
     """ create a tmp directory for storing output from test"""
@@ -33,37 +50,49 @@ def teardown_module():
     """ delete the symbolic link 
     delete the tmp directory if it is empty"""
     print "---------teardown_module------------"
-    if (os.path.isfile(XYTS_FILE)):
+    if os.path.isfile(XYTS_FILE):
         os.remove(XYTS_FILE)
-    if (len(os.listdir(TMP_DIR_NAME)) == 0):
+    if len(os.listdir(TMP_DIR_NAME)) == 0:
         try:
             shutil.rmtree(TMP_DIR_NAME)
         except (IOError, OSError) as (e):
             sys.exit(e)
 
 
-@pytest.mark.parametrize("gmt_format, expected_corners",[(False,[[170.9143829345703, -43.291595458984375], [172.26246643066406, -44.217742919921875], [171.01295471191406, -45.15299987792969], [169.66343688964844, -44.21214294433594]]) ,\
-(True,([[170.9143829345703, -43.291595458984375], [172.26246643066406, -44.217742919921875], [171.01295471191406, -45.15299987792969], [169.66343688964844, -44.21214294433594]], '170.914382935 -43.291595459\n172.262466431 -44.2177429199\n171.012954712 -45.1529998779\n169.66343689 -44.2121429443')
-)])
+@pytest.mark.parametrize("gmt_format, expected_corners", [(False, [[170.9143829345703, -43.291595458984375],
+                                                                   [172.26246643066406, -44.217742919921875],
+                                                                   [171.01295471191406, -45.15299987792969],
+                                                                   [169.66343688964844, -44.21214294433594]]), \
+                                                          (True, ([[170.9143829345703, -43.291595458984375],
+                                                                   [172.26246643066406, -44.217742919921875],
+                                                                   [171.01295471191406, -45.15299987792969],
+                                                                   [169.66343688964844, -44.21214294433594]],
+                                                                  '170.914382935 -43.291595459\n172.262466431 -44.2177429199\n171.012954712 -45.1529998779\n169.66343689 -44.2121429443')
+                                                           )])
 def test_corners(gmt_format, expected_corners):
-   assert OBJ_XYTS.corners(gmt_format=gmt_format)  == expected_corners
+    assert OBJ_XYTS.corners(gmt_format=gmt_format) == expected_corners
 
 
-@pytest.mark.parametrize("corners, expected_region",[(None,(169.66343688964844, 172.26246643066406, -45.15299987792969, -43.291595458984375)) ,\
-([[170.91449, -43.2916], [172.26257, -44.21773], [171.01305, -45.15298], [169.66354, -44.21213]],(169.66354000000001, 172.26257000000001, -45.152979999999999, -43.291600000000003))])
+@pytest.mark.parametrize("corners, expected_region",
+                         [(None, (169.66343688964844, 172.26246643066406, -45.15299987792969, -43.291595458984375)), \
+                          ([[170.91449, -43.2916], [172.26257, -44.21773], [171.01305, -45.15298],
+                            [169.66354, -44.21213]],
+                           (169.66354000000001, 172.26257000000001, -45.152979999999999, -43.291600000000003))])
 def test_region(corners, expected_region):
     assert OBJ_XYTS.region(corners) == expected_region
 
 
-@pytest.mark.parametrize("mmi, pgvout, mmiout, sample_pgv, sample_mmi",[(True, None, None, SAMPLE_PGV, SAMPLE_MMI),(False, None, None, SAMPLE_PGV, SAMPLE_MMI), \
-                                                                        (True, "test_pgv_path1", "test_mmi_path1", SAMPLE_PGV, SAMPLE_MMI),(False, "test_pgv_path2", None, SAMPLE_PGV, SAMPLE_MMI)])
+@pytest.mark.parametrize("mmi, pgvout, mmiout, sample_pgv, sample_mmi",
+                         [(True, None, None, SAMPLE_PGV, SAMPLE_MMI), (False, None, None, SAMPLE_PGV, SAMPLE_MMI), \
+                          (True, "test_pgv_path1", "test_mmi_path1", SAMPLE_PGV, SAMPLE_MMI),
+                          (False, "test_pgv_path2", None, SAMPLE_PGV, SAMPLE_MMI)])
 def test_pgv(mmi, pgvout, mmiout, sample_pgv, sample_mmi):
     files_to_del = []
     if pgvout:
-        pgvout = os.path.join(TMP_DIR_NAME,pgvout)
+        pgvout = os.path.join(TMP_DIR_NAME, pgvout)
         files_to_del.append(pgvout)
     if mmiout:
-        mmiout = os.path.join(TMP_DIR_NAME,mmiout)
+        mmiout = os.path.join(TMP_DIR_NAME, mmiout)
         files_to_del.append(mmiout)
 
     xyts_test_output_array = OBJ_XYTS.pgv(mmi=mmi, pgvout=pgvout, mmiout=mmiout)
@@ -71,7 +100,7 @@ def test_pgv(mmi, pgvout, mmiout, sample_pgv, sample_mmi):
     if pgvout:
         sample_pgv_array = np.fromfile(sample_pgv, dtype='3<f4')
         test_pgvout_array = np.fromfile(pgvout, dtype='3<f4')
-        utils.compare_np_array(sample_pgv_array,test_pgvout_array)
+        utils.compare_np_array(sample_pgv_array, test_pgvout_array)
         if mmiout:
             sample_mmi_array = np.fromfile(sample_mmi, dtype='3<f4')
             test_mmiout_array = np.fromfile(mmiout, dtype='3<f4')
@@ -81,7 +110,7 @@ def test_pgv(mmi, pgvout, mmiout, sample_pgv, sample_mmi):
             sample_pgv_array = np.fromfile(sample_pgv, dtype='3<f4')
             utils.compare_np_array(sample_pgv_array, xyts_test_output_array)
         elif mmiout == None:
-            pgv,mmi = xyts_test_output_array
+            pgv, mmi = xyts_test_output_array
             sample_pgv_array = np.fromfile(sample_pgv, dtype='3<f4')
             utils.compare_np_array(sample_pgv_array, pgv)
             sample_mmi_array = np.fromfile(sample_mmi, dtype='3<f4')
@@ -91,9 +120,11 @@ def test_pgv(mmi, pgvout, mmiout, sample_pgv, sample_mmi):
         utils.remove_file(f)
 
 
-@pytest.mark.parametrize("step, comp, test_outfile, sample_outfile",[(10, -1, None, "out_tslice-1"),\
-                        (10, -1,"test_tslice-1", "out_tslice-1"),(10, 0, "test_tslice0", "out_tslice0"), \
-                        (10, 1, "test_tslice1", "out_tslice1"),(10, 2, "test_tslice2", "out_tslice2")])
+@pytest.mark.parametrize("step, comp, test_outfile, sample_outfile", [(10, -1, None, "out_tslice-1"), \
+                                                                      (10, -1, "test_tslice-1", "out_tslice-1"),
+                                                                      (10, 0, "test_tslice0", "out_tslice0"), \
+                                                                      (10, 1, "test_tslice1", "out_tslice1"),
+                                                                      (10, 2, "test_tslice2", "out_tslice2")])
 def test_tslice_get(step, comp, test_outfile, sample_outfile):
     files_to_del = []
     if test_outfile:
@@ -109,8 +140,3 @@ def test_tslice_get(step, comp, test_outfile, sample_outfile):
             files_to_del.append(test_outfile)
     for f in files_to_del:
         utils.remove_file(f)
-
-
-
-
-
