c63467b3b14453ac94ba44150ec5b6bd45e55dc2
claudio <claudio.schill@canterbury.ac.nz>
2019-01-18 12:40:20 +1300

    python3 compatibility

--- qcore/pool_wrapper.py
+++ qcore/pool_wrapper.py
@@ -13,7 +13,7 @@ class PoolWrapper:
     def map(self, function, iterable_list):
         result = []
         if self.pool is None:
-            print "Ignoring number of processes here, executing for loop"
+            print("Ignoring number of processes here, executing for loop")
             for item in iterable_list:
                 result.append(function(item))
         else:
