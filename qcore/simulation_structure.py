46439aa3ab5197db12241dfcc02100108f247828
jpa198 <james.paterson@canterbury.ac.nz>
2019-01-28 14:15:55 +1300

    Changed sim struct functions to allow for out_folder style usage

--- qcore/simulation_structure.py
+++ qcore/simulation_structure.py
@@ -13,16 +13,28 @@ def get_realisation_name(fault_name, rel_no):
     return "{}_REL{:0>2}".format(fault_name, rel_no)
 
 
-def get_srf_path(cybershake_root, realisation):
+def get_srf_location(realisation):
     fault = __get_fault_from_realisation(realisation)
-    return os.path.join(cybershake_root, 'Data', 'Sources', fault, 'Srf', realisation + '.srf')
+    return os.path.join(fault, 'Srf', realisation + '.srf')
+
+
+def get_stoch_location(realisation):
+    fault = __get_fault_from_realisation(realisation)
+    return os.path.join(fault, 'Stoch', realisation + '.stoch')
+
+
+def get_source_params_location(realisation):
+    fault = __get_fault_from_realisation(realisation)
+    return os.path.join(fault, 'Sim_params', realisation + '.yaml')
+
+
+def get_srf_path(cybershake_root, realisation):
+    return os.path.join(cybershake_root, 'Data', 'Sources', get_srf_location(realisation))
 
 
 def get_stoch_path(cybershake_root, realisation):
-    fault = __get_fault_from_realisation(realisation)
-    return os.path.join(cybershake_root, 'Data', 'Sources', fault, 'Stoch', realisation + '.stoch')
+    return os.path.join(cybershake_root, 'Data', 'Sources', get_stoch_location(realisation))
 
 
 def get_source_params_path(cybershake_root, realisation):
-    fault = __get_fault_from_realisation(realisation)
-    return os.path.join(cybershake_root, 'Data', 'Sources', fault, 'Sim_params', realisation + '.yaml')
+    return os.path.join(cybershake_root, 'Data', 'Sources', get_source_params_location(realisation))
