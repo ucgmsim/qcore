470e10e581c1141afbccf70f383841caad803ab5
ttgenius <melodyzyz@gmail.com>
2019-01-31 15:25:17 +1300

    Added opt_dir in each config.json,added get_tools_dir to config.py

--- qcore/config.py
+++ qcore/config.py
@@ -19,3 +19,46 @@ config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
 
 with open(config_file, 'r') as f:
     qconfig = json.load(f)
+
+
+def get_host_config():
+    """
+    Determine the actual hostname eg.ni0002--> maui; maui01-->maui
+    :return: actual hostname, config josn file path
+    """
+    host_name = platform.node()
+
+    if host_name.startswith("ni") and len(host_name) == 8:  # maui
+        host_name = "maui"
+        base_name = os.path.join('machine_config', 'config_maui.json')
+
+    elif host_name.startswith("mahuika") and len(host_name) == 6:  # mahuika
+        host_name = "mahuika"
+        base_name = os.path.join('machine_config', 'config_mahuika.json')
+
+    else:  # default
+        base_name = "config.json"
+
+    config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), base_name)
+    return host_name, config_path
+
+
+def get_tools_dir(bin_name='emod3d', version='3.0.4-gcc'):
+    """
+    first get opt_dir from machine config based on host_name
+    then dynamically determine the tools_dir based on host_name, bin_name and version
+    :param bin_name: emod3d, hf, bb
+    :param version: eg. 3.0.3-gcc
+    :return: tools_dir path
+    """
+    host_name, config_path = get_host_config()
+    with open(config_path, 'r') as f:
+        config_data = json.load(f)
+
+    if host_name == 'maui' or host_name == 'mahuika':
+        opt_dir = json.load(f)['opt_dir']
+        tools_dir = os.path.join(opt_dir, host_name, bin_name, version, 'bin')
+    else:  # default
+        tools_dir = config_data['tools_dir']
+
+    return tools_dir
