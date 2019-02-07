bf99e08328df30fadb0542973b08e4561d565fc8
claudio <claudio.schill@canterbury.ac.nz>
2019-02-07 15:08:08 +1300

    Some updates to sim structure file and moved dashbord. Start on cybershake progress script.

--- qcore/simulation_structure.py
+++ qcore/simulation_structure.py
@@ -29,12 +29,30 @@ def get_source_params_location(realisation):
 
 
 def get_srf_path(cybershake_root, realisation):
-    return os.path.join(cybershake_root, 'Data', 'Sources', get_srf_location(realisation))
+    return os.path.join(get_sources_dir(cybershake_root), get_srf_location(realisation))
 
+def get_sources_dir(cybershake_root):
+    """Gets the cybershake sources directory"""
+    return os.path.join(cybershake_root, 'Data', 'Sources')
 
 def get_stoch_path(cybershake_root, realisation):
     return os.path.join(cybershake_root, 'Data', 'Sources', get_stoch_location(realisation))
 
-
 def get_source_params_path(cybershake_root, realisation):
     return os.path.join(cybershake_root, 'Data', 'Sources', get_source_params_location(realisation))
+
+def get_VMs_dir(cybershake_root):
+    """Gets the cybershake VMs directory"""
+    return os.path.join(cybershake_root, "Data","VMs")
+
+def get_runs_dir(cybershake_root):
+    """Gets the path to the Runs directory of a cybershake run"""
+    return os.path.join(cybershake_root, "Runs")
+
+def get_cybershake_config(cybershake_root):
+    """Gets the path to the cybershake config json file"""
+    return os.path.join(cybershake_root, "cybershake_config.json")
+
+def get_cybershake_list(cybershake_root):
+    """Gets the cybershake list, specifying the faults and number of realisation"""
+    return os.path.join(cybershake_root, "list.txt")
