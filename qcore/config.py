47792c6a6ad09663706b6f616da243453e8e07cd
claudio <claudio.schill@canterbury.ac.nz>
2019-02-11 10:59:21 +1300

    Config.py fix

--- qcore/config.py
+++ qcore/config.py
@@ -1,26 +1,6 @@
-
 import json
 import os
 import platform
-
-hostname = platform.node()
-if hostname.startswith("ni") and len(hostname) == 8:
-    host = "maui"
-    basename = os.path.join('machine_config', 'config_maui.json')
-elif hostname.startswith("wb") and len(hostname) == 6:
-    host = "mahuika"
-    basename = os.path.join('machine_config', 'config_mahuika.json')
-else:
-    # default
-    host = "unknown"
-    basename = 'config.json'
-
-config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-        basename)
-
-with open(config_file, 'r') as f:
-    qconfig = json.load(f)
-
 
 def get_host_config():
     """
@@ -32,7 +12,7 @@ def get_host_config():
         actual_host_name = "maui"
         base_name = os.path.join('machine_config', 'config_maui.json')
 
-    elif host_name.startswith("mahuika") and len(host_name) == 6:  # mahuika
+    elif (host_name.startswith("wb") and len(host_name) == 6) or host_name.startswith("mahuika"):  # mahuika
         actual_host_name = "mahuika"
         base_name = os.path.join('machine_config', 'config_mahuika.json')
 
@@ -44,22 +24,9 @@ def get_host_config():
     return actual_host_name, config_path
 
 
-def get_tools_dir(bin_name, version='3.0.4-gcc'):
-    """
-    first get opt_dir from machine config based on host_name
-    then dynamically determine the tools_dir based on host_name, bin_name and version
-    :param bin_name: emod3d, hf, bb
-    :param version: eg. 3.0.3-gcc
-    :return: tools_dir path
-    """
-    host_name, config_path = get_host_config()
-    with open(config_path, 'r') as f:
-        config_data = json.load(f)
+host, config = get_host_config()
+config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+        config)
 
-    if host_name == 'maui' or host_name == 'mahuika':
-        opt_dir = config_data['opt_dir']
-        tools_dir = os.path.join(opt_dir, host_name, bin_name, version, 'bin')
-    else:  # default
-        tools_dir = config_data['tools_dir']
-
-    return tools_dir
+with open(config_file, 'r') as f:
+    qconfig = json.load(f)
