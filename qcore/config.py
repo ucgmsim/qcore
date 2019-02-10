7b8bfc1ff50de22407d53c64b9a0f40ad9a409e6
ttgenius <melodyzyz@gmail.com>
2019-02-10 22:00:58 +0000

    func to load qconfig

--- qcore/config.py
+++ qcore/config.py
@@ -3,19 +3,33 @@ import json
 import os
 import platform
 
-hostname = platform.node()
-if hostname.startswith("ni") and len(hostname) == 8 or hostname.startswith('maui'):
-    # maui
-    basename = os.path.join('machine_config', 'config_maui.json')
-elif hostname.startswith("wb") and len(hostname) == 6 or hostname.startswith('mahuika'):
-    # mahuika
-    basename = os.path.join('machine_config', 'config_mahuika.json')
-else:
-    # default
-    basename = 'config.json'
 
+def get_host_config():
+    """
+    Determine the actual hostname eg.ni0002--> maui; maui01-->maui
+    :return: actual hostname, config josn file path
+    """
+    host_name = platform.node()
+    if (host_name.startswith("ni") and len(host_name) == 8) or host_name.startswith('maui'):  # maui
+        actual_host_name = "maui"
+        base_name = os.path.join('machine_config', 'config_maui.json')
+
+    elif (host_name.startswith("wb") and len(host_name) == 6) or host_name.startswith("mahuika"):  # mahuika
+        actual_host_name = "mahuika"
+        base_name = os.path.join('machine_config', 'config_mahuika.json')
+
+    else:  # default
+        actual_host_name = "default"
+        base_name = "config.json"
+
+    config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), base_name)
+    return actual_host_name, config_path
+
+
+host, config = get_host_config()
 config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-        basename)
+        config)
 
 with open(config_file, 'r') as f:
     qconfig = json.load(f)
+
