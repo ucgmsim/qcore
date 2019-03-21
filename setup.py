95f91b6142d0af70baab9e563df00ddf427c56ea
Sung Bae <sung.bae@nesi.org.nz>
2019-03-21 11:14:55 +0000

    Added version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='qcore',
-    version='1.1.2',
+    version='1.1.3',
     packages=['qcore'],
     url='https://github.com/ucgmsim/qcore',
     description='QuakeCoRE Library',
