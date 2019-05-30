e44333cbe82a86d1cdf38cddf54032c90adb4a6f
claudio <claudio.schill@canterbury.ac.nz>
2019-05-31 08:56:01 +1200

    Some improvements as per PR

--- qcore/nhm.py
+++ qcore/nhm.py
@@ -16,8 +16,12 @@ class NHMInfo:
     dip_mean : float  (deg)
     dip_sigma : float (deg)
     dip_dir : float
+    rake : float
     rup_depth_mean : float (km)
     rup_depth_sigma : float (km)
+    rup_top_mean : float (km)
+    rup_top_min : float (km)
+    rup_top_max : float (km)
     slip_rate_mean : float (mm/yr)
     slip_rate_sigma : float (mm/yr)
     coupling_coeff : float (mm/yr)
@@ -30,10 +34,53 @@ class NHMInfo:
         The fault surface locations, shape [n_locations, 2], format (lon, lat)
     """
 
-    def __init__(self):
-        """Do not use the constructor, instances should only be
-        created via the from_nhm_section method"""
-        pass
+    def __init__(
+        self,
+        fault_name: str,
+        tectonic_type: str,
+        fault_type: str,
+        length_mean: float,
+        length_sigma: float,
+        dip_mean: float,
+        dip_sigma: float,
+        dip_dir: float,
+        rake: float,
+        rup_depth_mean: float,
+        rup_depth_sigma: float,
+        rup_top_mean: float,
+        rup_top_min: float,
+        rup_top_max: float,
+        slip_rate_mean: float,
+        slip_rate_sigma: float,
+        coupling_coeff: float,
+        coupling_coeff_sigma: float,
+        mw_median: float,
+        recur_int_median: float,
+        n_locations: int,
+        locations: np.ndarray,
+    ):
+        self.fault_name = fault_name
+        self.tectonic_type = tectonic_type
+        self.fault_type = fault_type
+        self.length_mean = length_mean
+        self.length_sigma = length_sigma
+        self.dip_mean = dip_mean
+        self.dip_sigma = dip_sigma
+        self.dip_dir = dip_dir
+        self.rake = rake
+        self.rup_depth_mean = rup_depth_mean
+        self.rup_depth_sigma = rup_depth_sigma
+        self.rup_top_mean = rup_top_mean
+        self.rup_top_min = rup_top_min
+        self.rup_top_max = rup_top_max
+        self.slip_rate_mean = slip_rate_mean
+        self.slip_rate_sigma = slip_rate_sigma
+        self.coupling_coeff = coupling_coeff
+        self.coupling_coeff_sigma = coupling_coeff_sigma
+        self.mw_median = mw_median
+        self.recur_int_median = recur_int_median
+        self.n_locations = n_locations
+        self.locations = locations
 
     @classmethod
     def from_nhm_section(cls, rows: List[str]):
@@ -62,29 +109,45 @@ class NHMInfo:
         -------
         NHMInfo
         """
-        # Not sure of a tidier of doing this (without adding all these to the init..)
-        info = NHMInfo()
+        fault_name = rows[0].strip()
+        tectonic_type, fault_type = cls.__read_row(rows[1], convert=False)
+        length_mean, length_sigma = cls.__read_row(rows[2])
+        dip_mean, dip_sigma = cls.__read_row(rows[3])
+        dip_dir = float(rows[4].strip())
+        rake = float(rows[5].strip())
+        rup_depth_mean, rup_depth_sigma = cls.__read_row(rows[6])
+        rup_top_mean, rup_top_min, rup_top_max = cls.__read_row(rows[7])
+        slip_rate_mean, slip_rate_sigma = cls.__read_row(rows[8])
+        coupling_coeff, coupling_coeff_sigma = cls.__read_row(rows[9])
+        mw_median, recur_int_median = cls.__read_row(rows[10])
+        n_locations = int(rows[11].strip())
 
-        info.fault_name = rows[0].strip()
-        info.tectonic_type, info.fault_type = cls.read_row(rows[1], convert=False)
-        info.length_mean, info.length_sigma = cls.read_row(rows[2])
-        info.dip_mean, info.dip_sigma = cls.read_row(rows[3])
-        info.dip_dir = float(rows[4].strip())
-        info.rake = float(rows[5].strip())
-        info.rup_depth_mean, info.rup_depth_sigma = cls.read_row(rows[6])
-        info.rup_top_mean, info.rup_top_min, info.rup_top_max = cls.read_row(rows[7])
-        info.slip_rate_mean, info.slip_rate_sigma = cls.read_row(rows[8])
-        info.coupling_coeff, info.coupling_coeff_sigma = cls.read_row(rows[9])
-        info.mw_median, info.recur_int_median = cls.read_row(rows[10])
-        info.n_locations = int(rows[11].strip())
-
-        # Read the locations
-        info.locations = np.asarray([cls.read_row(row) for row in rows[12:]])
-
-        return info
+        return NHMInfo(
+            fault_name,
+            tectonic_type,
+            fault_type,
+            length_mean,
+            length_sigma,
+            dip_mean,
+            dip_sigma,
+            dip_dir,
+            rake,
+            rup_depth_mean,
+            rup_depth_sigma,
+            rup_top_mean,
+            rup_top_min,
+            rup_top_max,
+            slip_rate_mean,
+            slip_rate_sigma,
+            coupling_coeff,
+            coupling_coeff_sigma,
+            mw_median,
+            recur_int_median,
+            n_locations,
+            np.asarray([cls.__read_row(row) for row in rows[12:]]))
 
     @staticmethod
-    def read_row(row: str, sep: str = None, convert: bool = True):
+    def __read_row(row: str, sep: str = None, convert: bool = True):
         return [
             float(entry.strip()) if convert else entry.strip()
             for entry in row.split(sep)
