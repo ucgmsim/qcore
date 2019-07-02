6872b3dd708147ffe8de414297c1576ab31589e0
jpa198 <james.paterson@canterbury.ac.nz>
2019-07-02 16:55:58 +1200

    Fix a bug in simulation structure

--- qcore/simulation_structure.py
+++ qcore/simulation_structure.py
@@ -190,7 +190,7 @@ def get_fault_yaml_path(sim_root, fault_name=None):
     fault params are stored directly in the simulation directory.
     """
     fault_name = "" if fault_name is None else fault_name
-    return os.path.join(sim_root, fault_name, "fault_params.yaml")
+    return os.path.join(get_runs_dir(sim_root), fault_name, "fault_params.yaml")
 
 
 def get_root_yaml_path(sim_root):
