30fd7e8d1437c03f69382596dbe33f50017920fc
jpa198 <james.paterson@canterbury.ac.nz>
2019-08-08 10:58:08 +1200

    Added star map to pool wrapper so each script that uses it doesn't have to reinvent the wheel

--- qcore/pool_wrapper.py
+++ qcore/pool_wrapper.py
@@ -21,6 +21,17 @@ class PoolWrapper:
 
         return result
 
+    def starmap(self, function, iterable_list):
+        result = []
+        if self.pool is None:
+            print("Ignoring number of processes here, executing for loop")
+            for item in iterable_list:
+                result.append(function(*item))
+        else:
+            result = self.pool.starmap(function, iterable_list)
+
+        return result
+
 
 
 
