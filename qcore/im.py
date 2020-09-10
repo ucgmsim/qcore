9409794c1ca445f3f9537a36b3a8c2d7c0321778
sdw123123@gmail.com <sdw123123@gmail.com>
2020-09-11 11:30:21 +1200

    Add another function to sort ims as per the pattern order given

--- qcore/im.py
+++ qcore/im.py
@@ -68,11 +68,13 @@ def order_im_cols_df(df, pattern_order=default_pattern_order):
                 for col in cur_cols:
                     values.extend(
                         (
-                            list(float(col.split("_")[value_ix].replace("p", "."))
-                            for value_ix in float_cols)
+                            list(
+                                float(col.split("_")[value_ix].replace("p", "."))
+                                for value_ix in float_cols
+                            )
                         )
                     )
-                
+
                 sorted_indices = np.argsort(values)
 
             # Otherwise just sort by length of the column name
@@ -88,3 +90,58 @@ def order_im_cols_df(df, pattern_order=default_pattern_order):
         [adj_cols.append(col) for col in orig_cols if col not in adj_cols]
 
     return df[adj_cols]
+
+
+def order_ims(unsorted_ims, pattern_order=default_pattern_order):
+    """
+    Orders the ims as per the pattern order given.
+    """
+    orig_ims = unsorted_ims
+    adj_ims = []
+
+    for pattern in pattern_order:
+        cur_cols = [col for col in orig_ims if col.startswith(pattern)]
+
+        if len(cur_cols) == 0:
+            continue
+        elif len(cur_cols) == 1:
+            adj_ims.append(cur_cols[0])
+        else:
+            # Check if column name contains a valid float value,
+            # e.g. pSA_0.5_epsilon.
+            float_cols = []
+            for ix, split in enumerate(cur_cols[0].split("_")):
+                try:
+                    float(split.replace("p", "."))
+                    float_cols.append(ix)
+                except ValueError:
+                    continue
+
+            if len(float_cols) > 0:
+                # Get the values (as the list is sorted on those)
+                values = []
+                for col in cur_cols:
+                    values.extend(
+                        (
+                            list(
+                                float(col.split("_")[value_ix].replace("p", "."))
+                                for value_ix in float_cols
+                            )
+                        )
+                    )
+
+                sorted_indices = np.argsort(values)
+
+            # Otherwise just sort by length of the column name
+            else:
+                sorted_indices = np.argsort([len(col) for col in cur_cols])
+
+            # Sort the columns names
+            adj_ims = adj_ims + list(np.asarray(cur_cols)[sorted_indices])
+
+    # Deal with columns that aren't handled by the pattern.
+    # These are just added to the end, in the original order
+    if len(adj_ims) != len(orig_ims):
+        [adj_ims.append(col) for col in orig_ims if col not in adj_ims]
+
+    return adj_ims
