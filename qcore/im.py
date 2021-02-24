ffe9b72fc3df6c789e1ec1c04313b8dab8e494cd
Jason <25143301+jasonmotha@users.noreply.github.com>
2021-02-25 12:01:01 +1300

    deleted unused files and created im dataclass

--- qcore/im.py
+++ qcore/im.py
@@ -3,8 +3,12 @@ Correct IM column order
 station, component, PGA*, PGV*, CAV*, AI*, Ds*, MMI*, pSA_*, FAS_*, IESDR_*
 """
 
+from dataclasses import dataclass
+
 import pandas as pd
 import numpy as np
+
+from qcore import constants
 
 DEFAULT_PATTERN_ORDER = (
     "station",
@@ -21,6 +25,20 @@ DEFAULT_PATTERN_ORDER = (
     "FAS",
     "IESDR",
 )
+
+
+class IMEnum(constants.ExtendedEnum):
+    PGA = 1
+    PGV = 2
+    CAV = 3
+    AI = 4
+    Ds575 = 5
+    Ds595 = 6
+    Ds2080 = 7
+    MMI = 8
+    pSA = 9
+    FAS = 10
+    IESDR = 11
 
 
 def order_im_cols_file(filename):
@@ -99,3 +117,48 @@ def order_ims(unsorted_ims, pattern_order=DEFAULT_PATTERN_ORDER):
         [adj_ims.append(im) for im in unsorted_ims if im not in adj_ims]
 
     return adj_ims
+
+
+@dataclass
+class IM:
+    name: [IMEnum] = None
+    period: [int] = None
+    component: [constants.Components] = None
+
+    def get_im_name(self):
+        if self.period:
+            return f"{self.name}_{self.period}"
+        else:
+            return self.name
+
+    def pretty_im_name(self):
+        if self.period:
+            return f"{self.name} ({self.period}{self.get_period_unit()}) [{self.get_unit()}]"
+        else:
+            return f"{self.name} [{self.get_unit()}]"
+
+    def get_unit(self):
+        if self.name in ["PGA", "pSA"]:
+            return "g"
+        elif self.name == "PGV":
+            return "cm/s"
+        elif self.name == "CAV":
+            return "gs"
+        elif self.name == "AI":
+            return "cm/s"
+        elif self.name == "MMI":
+            return ""  # MMI is dimensionless
+        elif self.name == "FAS":
+            return "gs"
+        elif self.name == "IESDR":
+            return ""
+        elif self.name.startswith("Ds"):
+            return "s"
+        else:
+            return ""  # unimplemented
+
+    def get_period_unit(self):
+        if self.name in ["pSA", "IESDR"]:
+            return "s"
+        elif self.name == "FAS":
+            return "Hz"
