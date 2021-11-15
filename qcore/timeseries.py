6454b6946fa151170213bf5250d0c165a21acbaf
James Paterson <46693562+jpa198@users.noreply.github.com>
2021-11-16 12:22:35 +1300

    Add single frequency bandpass to bwfilter

--- qcore/timeseries.py
+++ qcore/timeseries.py
@@ -57,6 +57,8 @@ def bwfilter(data, dt, freq, band, match_powersb=True):
     if match_powersb:
         if band == "highpass":
             freq *= 0.8956803352330285
+        elif band == "bandpass":
+            freq = np.asarray((freq*0.8956803352330285, freq*1.1164697500474103))
         else:
             freq *= 1.1164697500474103
     return sosfiltfilt(
