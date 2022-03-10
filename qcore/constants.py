ef250746031d8ec1f06d6bd7afdc4317cd2319de
joelridden <jriddenw@gmail.com>
2022-03-10 17:06:34 +1300

    Adding WCT as a Status Enum

--- qcore/constants.py
+++ qcore/constants.py
@@ -274,7 +274,8 @@ class Status(ExtendedStrEnum):
     running = 3, "running"
     unknown = 4, "unknown"
     completed = 5, "completed"
-    failed = 6, "failed"
+    WCT = 6, "WCT"
+    failed = 7, "failed"
 
 
 class RootParams(Enum):
