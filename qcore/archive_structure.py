7a2a35604853c16baad86caefbe9e8009984af88
joelridden <jriddenw@gmail.com>
2023-03-15 14:28:50 +1300

    Black

--- qcore/archive_structure.py
+++ qcore/archive_structure.py
@@ -5,10 +5,15 @@ from pathlib import Path
 
 
 def get_fault_from_realisation(realisation: str):
-    """ Gets the fault name from the realisation name"""
+    """Gets the fault name from the realisation name"""
     return realisation.rsplit("_REL", 1)[0]
 
 
 def get_IM_csv_from_root(archive_root: Path, realisation: str):
-    """ Gets the full path to the im_csv file given the archive root dir and the realistion name"""
-    return archive_root / get_fault_from_realisation(realisation) / "IM" / f"{realisation}.csv"
+    """Gets the full path to the im_csv file given the archive root dir and the realistion name"""
+    return (
+        archive_root
+        / get_fault_from_realisation(realisation)
+        / "IM"
+        / f"{realisation}.csv"
+    )
