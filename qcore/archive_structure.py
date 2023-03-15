0aec5b89704b36176dabc9e27c91b54c31db34a1
joelridden <jriddenw@gmail.com>
2023-03-15 14:44:36 +1300

    PR Comments

--- qcore/archive_structure.py
+++ qcore/archive_structure.py
@@ -3,10 +3,7 @@ Gives access to the folder structure of archived cybershake directories
 """
 from pathlib import Path
 
-
-def get_fault_from_realisation(realisation: str):
-    """Gets the fault name from the realisation name"""
-    return realisation.rsplit("_REL", 1)[0]
+from simulation_structure import get_fault_from_realisation
 
 
 def get_IM_csv_from_root(archive_root: Path, realisation: str):
