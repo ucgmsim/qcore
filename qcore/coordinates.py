08990042f8ef1ef1ba38f4a84dec9d0eddb8aeb8
Jake Faulkner <jake.faulkner@canterbury.ac.nz>
2024-05-29 16:27:00 +1200

    improve docstrings

--- qcore/coordinates.py
+++ qcore/coordinates.py
@@ -29,12 +29,21 @@ def wgs_depth_to_nztm(wgs_depth_coordinates: np.ndarray) -> np.ndarray:
     Parameters
     ----------
     wgs_depth_coordinates : np.ndarray
-        An array of shape (N, 3) containing WGS84 coordinates (latitude, longitude, depth).
+        An array of shape (N, 3) or (3,) containing WGS84 coordinates
+        (latitude, longitude, depth).
 
     Returns
     -------
     np.ndarray
         An array of shape (N, 3) containing NZTM coordinates (x, y, depth).
+
+    Examples
+    --------
+    >>> wgs_depth_to_nztm(np.array([-43.5320, 172.6366, 0]))
+    array([5180040.61473068, 1570636.6812821 ,       0.        ])
+    >>> wgs_depth_to_nztm(np.array([[-36.8509, 174.7645, 100], [-41.2924, 174.7787, 100]]))
+    array([[5.92021456e+06, 1.75731133e+06, 1.00000000e+02],
+           [5.42725716e+06, 1.74893148e+06, 0.00000000e+00]])
     """
     if wgs_depth_coordinates.shape == (3,):
         return np.array(_WGS2NZTM.transform(*wgs_depth_coordinates))
@@ -54,12 +63,20 @@ def nztm_to_wgs_depth(nztm_coordinates: np.ndarray) -> np.ndarray:
     Parameters
     ----------
     nztm_coordinates : np.ndarray
-        An array of shape (N, 3) containing NZTM coordinates (x, y, depth).
+        An array of shape (N, 3) or (3,) containing NZTM coordinates (x, y, depth).
 
     Returns
     -------
     np.ndarray
         An array of shape (N, 3) containing WGS84 coordinates (latitude, longitude, depth).
+
+    Examples
+    --------
+    >>> nztm_to_wgs_depth(np.array([5180040.61473068, 1570636.6812821, 0]))
+    array([-43.5320, 172.6366, 0])
+    >>> wgs_depth_to_nztm(array([[5.92021456e+06, 1.75731133e+06, 100],
+                                 [5.42725716e+06, 1.74893148e+06, 0]]))
+    array([[-36.8509, 174.7645, 100], [-41.2924, 174.7787, 100]])
     """
     if nztm_coordinates.shape == (3,):
         return np.array(_NZTM2WGS.transform(*nztm_coordinates))
