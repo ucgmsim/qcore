9eccd5334afeb7f53d6afbebf43dfc19f0268961
sungeunbae <sung.bae@canterbury.ac.nz>
2024-08-06 16:50:59 +1200

    Test updated

--- qcore/test/test_source_site_dist/test_src_site_dist_calc.py
+++ qcore/test/test_source_site_dist/test_src_site_dist_calc.py
@@ -1,13 +1,64 @@
 import os
 import pickle
+import shutil
+import sys
 
 import numpy as np
 import pytest
 
 from qcore import geo, src_site_dist as ssd
+from qcore import shared
 
 INPUT = "input"
 OUTPUT = "output"
+REALISATIONS = [
+    (
+        "PangopangoF29_HYP01-10_S1244",
+        "https://qc-s3-autotest.s3-ap-southeast-2.amazonaws.com/testing/IM_calculation/PangopangoF29_HYP01-10_S1244.zip",
+    )
+]
+
+
+@pytest.yield_fixture(scope="session", autouse=True)
+def set_up(request):
+    test_data_save_dirs = []
+    for i, (REALISATION, DATA_DOWNLOAD_PATH) in enumerate(REALISATIONS):
+        data_store_path = os.path.join(os.getcwd(), "sample" + str(i))
+        zip_download_path = os.path.join(data_store_path, REALISATION + ".zip")
+
+        download_cmd = "wget -O {} {}".format(zip_download_path, DATA_DOWNLOAD_PATH)
+        unzip_cmd = "unzip {} -d {}".format(zip_download_path, data_store_path)
+        # print(DATA_STORE_PATH)
+        test_data_save_dirs.append(data_store_path)
+        if not os.path.isdir(data_store_path):
+            os.makedirs(data_store_path, exist_ok=True)
+            out, err = shared.exe(download_cmd, debug=False)
+            if "error" in err:
+                shutil.rmtree(data_store_path)
+                sys.exit("{} failed to retrieve test data".format(err))
+            # download_via_ftp(DATA_DOWNLOAD_PATH, zip_download_path)
+            if not os.path.isfile(zip_download_path):
+                sys.exit(
+                    "File failed to download from {}. Exiting".format(
+                        DATA_DOWNLOAD_PATH
+                    )
+                )
+            out, err = shared.exe(unzip_cmd, debug=False)
+            os.remove(zip_download_path)
+            if "error" in err:
+                shutil.rmtree(data_store_path)
+                sys.exit("{} failed to extract data folder".format(err))
+
+        else:
+            print("Benchmark data folder already exits: ", data_store_path)
+
+    # Run all tests
+    yield test_data_save_dirs
+
+    # Remove the test data directory
+    for PATH in test_data_save_dirs:
+        if os.path.isdir(PATH):
+            shutil.rmtree(PATH, ignore_errors=True)
 
 
 def test_calc_rrub_rjb(set_up):
