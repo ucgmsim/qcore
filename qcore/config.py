aff5d93dd391acdb8e60bb0bcb71dfe9a798814b
Sung Bae <sung.bae@nesi.org.nz>
2024-08-15 01:37:09 +0000

    To support Python 3.9

--- qcore/config.py
+++ qcore/config.py
@@ -27,7 +27,7 @@ from enum import Enum, auto
 from json import load
 from pathlib import Path
 from platform import node
-from typing import Optional, Tuple, TypedDict
+from typing import Optional, Tuple, TypedDict, Union
 
 
 class ConfigDict(TypedDict):
@@ -75,7 +75,7 @@ def determine_machine_config(hostname: str = node()) -> Tuple[str, str]:
 
 
 def get_machine_config(
-    hostname: str = node(), config_path: Optional[Path | str] = None
+    hostname: str = node(), config_path: Optional[Union[Path, str]] = None
 ) -> ConfigDict:
     """Get the current machine config.
 
