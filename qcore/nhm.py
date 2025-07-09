f771a0f016bb20a22960c188c562e0ed21683a81
Jake Faulkner <jake.faulkner@canterbury.ac.nz>
2025-07-10 10:42:04 +1200

    feat(nhm): automatically download NHM from Cybershake 200m if not available

--- qcore/nhm.py
+++ qcore/nhm.py
@@ -1,29 +1,32 @@
-from typing import List, Union, Dict
+"""NHM Model Module."""
+
 import datetime
 import math
 from dataclasses import dataclass
+from typing import TextIO
 
 import numpy as np
 import pandas as pd
+import pooch
 
+from qcore import geo
 from qcore.uncertainties import mag_scaling
 from qcore.uncertainties.distributions import truncated_normal as sample_trunc_norm_dist
-from qcore import geo
 
-NHM_HEADER = f"""FAULT SOURCES - (created {datetime.datetime.now().strftime("%d-%b-%Y")}) 
-Row 1: FaultName 
-Row 2: TectonicType , FaultType 
-Row 3: LengthMean , LengthSigma (km) 
-Row 4: DipMean , DipSigma (deg) 
-Row 5: DipDir 
-Row 6: Rake (deg) 
-Row 7: RupDepthMean , RupDepthSigma (km) 
-Row 8: RupTopMean, RupTopMin RupTopMax  (km) 
-Row 9: SlipRateMean , SlipRateSigma (mm/yr) 
-Row 10: CouplingCoeff , CouplingCoeffSigma (mm/yr) 
-Row 11: MwMedian , RecurIntMedian  (yr) 
-Row 12: Num Locations on Fault Surface 
-Row 13+: Location Coordinates (Long, Lat) 
+NHM_HEADER = f"""FAULT SOURCES - (created {datetime.datetime.now().strftime("%d-%b-%Y")})
+Row 1: FaultName
+Row 2: TectonicType , FaultType
+Row 3: LengthMean , LengthSigma (km)
+Row 4: DipMean , DipSigma (deg)
+Row 5: DipDir
+Row 6: Rake (deg)
+Row 7: RupDepthMean , RupDepthSigma (km)
+Row 8: RupTopMean, RupTopMin RupTopMax  (km)
+Row 9: SlipRateMean , SlipRateSigma (mm/yr)
+Row 10: CouplingCoeff , CouplingCoeffSigma (mm/yr)
+Row 11: MwMedian , RecurIntMedian  (yr)
+Row 12: Num Locations on Fault Surface
+Row 13+: Location Coordinates (Long, Lat)
 """
 
 # This mu is used to calculated Moment Rate
@@ -33,10 +36,14 @@ POINTS_PER_KILOMETER = (
     1 / 0.1
 )  # 1km divided by distance between points (1km/0.1km gives 100m grid)
 
+NHM_MODEL_URL = "https://www.dropbox.com/scl/fi/q93swheg6pqs0fiwviohg/NZ_FLTmodel_2010_v18p6.txt?rlkey=gnotizv4kx50un73y9176gzhk&st=667dlt19&dl=0"
+NHM_MODEL_HASH = "3e70bf86f00d89d8191b7e0d27d052e3c5784900e7bdc4963d3772e692305d7a"
+
 
 @dataclass
 class NHMFault:
-    """Contains the information for a single fault from a NHM file
+    """Contains the information for a single fault from a NHM file.
+
     Attributes
     ----------
     name : str
@@ -83,12 +90,12 @@ class NHMFault:
     coupling_coeff_sigma: float
     mw: float
     recur_int_median: float
-    trace: np.array
+    trace: np.ndarray
 
     # TODO: add x y z fault plane data as in SRF info
     # TODO: add leonard mw function
 
-    def sample_2012(self, mw_area_scaling=True, mw_perturbation=True):
+    def sample_2012(self, mw_area_scaling: bool = True, mw_perturbation: bool = True):
         """
         Permutates the current NHM fault as per the OpenSHA implementation. This uses the same Mw scaling relations
         as Stirling 2012
@@ -151,7 +158,7 @@ class NHMFault:
             trace=self.trace,
         )
 
-    def write(self, out_fp, header=False):
+    def write(self, out_fp: TextIO, header: bool = False):
         """
         Writes a section of the NHM file
 
@@ -179,7 +186,9 @@ class NHMFault:
             out_fp.write(f"{lat:10.5f} {lon:10.5f}\n")
 
 
-def load_nhm(nhm_path: str, skiprows: int = len(NHM_HEADER.splitlines()) + 1):
+def load_nhm(
+    nhm_path: str | None = None, skiprows: int = len(NHM_HEADER.splitlines()) + 1
+):
     """Reads the nhm_path and returns a dictionary of NHMFault by fault name.
 
     Parameters
@@ -193,6 +202,8 @@ def load_nhm(nhm_path: str, skiprows: int = len(NHM_HEADER.splitlines()) + 1):
     -------
     dict of NHMFault by name
     """
+    if not nhm_path:
+        nhm_path = pooch.retrieve(url=NHM_MODEL_URL, known_hash=NHM_MODEL_HASH)
     with open(nhm_path, "r") as f:
         rows = "".join(f.readlines()[skiprows:])
 
@@ -200,7 +211,7 @@ def load_nhm(nhm_path: str, skiprows: int = len(NHM_HEADER.splitlines()) + 1):
     for entry in rows.split("\n\n"):
         rows = list(map(str.strip, entry.split("\n")))
 
-        def str2floats(line):
+        def str2floats(line: str):
             return list(map(float, line.split()))
 
         tectonic_type, fault_type = rows[1].split()
@@ -243,7 +254,7 @@ def load_nhm(nhm_path: str, skiprows: int = len(NHM_HEADER.splitlines()) + 1):
     return faults
 
 
-def load_nhm_df(nhm_ffp, erf_name=None):
+def load_nhm_df(nhm_ffp: str, erf_name: str | None = None):
     """Creates a standardised pandas dataframe for the
     ruptures in the given erf file.
 
@@ -291,7 +302,7 @@ def load_nhm_df(nhm_ffp, erf_name=None):
             if info.recur_int_median > 0
             else float("nan"),
         }
-        for key, info in nhm_infos.items()
+        for info in nhm_infos.values()
     }
 
     return pd.DataFrame.from_dict(rupture_dict, orient="index").sort_index()
@@ -307,7 +318,7 @@ def get_fault_header_points(fault: NHMFault):
         A fault object from an NHM file
     """
     srf_points = []
-    srf_header: List[Dict[str, Union[int, float]]] = []
+    srf_header: list[dict[str, int | float]] = []
     lon1, lat1 = fault.trace[0]
     lon2, lat2 = fault.trace[1]
     strike = geo.ll_bearing(lon1, lat1, lon2, lat2, midpoint=True)
@@ -347,7 +358,7 @@ def get_fault_header_points(fault: NHMFault):
         for j in range(1, ndip):
             hdist = j * hdip_dist
             vdist = j * vdip_dist + fault.dtop
-            for local_lon, local_lat, local_depth in srf_points[
+            for local_lon, local_lat, _ in srf_points[
                 plane_offset : plane_offset + nstrike
             ]:
                 new_lat, new_lon = geo.ll_shift(
